587457ccf3489ef813f3f076ba8d7f84ae9db27c
Robert Santos <robertsantosti@outlook.com>
2023-07-14 19:41:28 -0300

    Criando Server no db.json

--- src/services/User/User.service.jsx
+++ src/services/User/User.service.jsx
@@ -1,9 +1,11 @@
 import { LocalStorageService } from "./LocalStorage.service";
 
+//Method GET
 const Get = () => {
   return LocalStorageService.get('users');
 }
 
+//Method POST
 const Create = (data) => {
   const users = Get();
 
@@ -15,18 +17,22 @@ const Create = (data) => {
   LocalStorageService.set('users', [...users, data]);
 }
 
+//Method GET
 const Show = (id) => {
   return Get().find(user => user.id === id);
 }
 
+//Method GET
 const ShowByEmail = (email) => {
   return Get().find(user => user.email === email);
 }
 
+//Method DELETE
 const Delete = (id) => {
   LocalStorageService.set('users', Get().filter(user => user.id !== id));
 }
 
+//Method PUT/PATCH
 const Update = (id, data) => {
   const users = Get();
 
@@ -34,7 +40,6 @@ const Update = (id, data) => {
 
   LocalStorageService.set('users', users)
 }
-
 
 export const UserService = {
   Get,
